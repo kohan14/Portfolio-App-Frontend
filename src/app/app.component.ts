6983f15e3865295b81a79ee0f1f31587ea4e10ac
kohan14 <63232710+kohan14@users.noreply.github.com>
2020-08-02 12:43:18 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,26 +6,6 @@ import { Component, HostListener } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'PortfolioApp';
-
-  inViewport : boolean = false;
-  
-  @HostListener("document : scroll")
-  onScrollReveal(divClass : string) {
-    var div = document.querySelector('.about-container');
-    var bounding = div.getBoundingClientRect();
-    console.log(bounding)
-    if(bounding.top >= 0 
-      && bounding.bottom <=(window.innerHeight || document.documentElement.clientHeight))
-    {
-      console.log("in viewport")
-      this.inViewport = true;
-    }
-    else
-    {
-      console.log("Not in viewport")
-      this.inViewport = false;
-    }
-  }
+  title = 'Piotr Kochanowski Portfolio';
 }
 
